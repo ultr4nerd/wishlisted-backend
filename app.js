984d3f79d6feb4d9033886065b6f59a4607daffe
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2020-06-21 14:00:19 -0500

    Try catch on promises

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ const app = express();
 app.use(cors());
 
 app.get('/', async (req, res) => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-  const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const page = await browser.newPage();
 
-  await page.goto(req.query.url, { waitUntil: 'domcontentloaded' });
+    await page.goto(req.query.url, { waitUntil: 'domcontentloaded' });
 
-  await page.setCacheEnabled(false);
-  const data = await page.evaluate(scrapper);
+    await page.setCacheEnabled(false);
+    const data = await page.evaluate(scrapper);
 
-  await browser.close();
-  res.json({ data });
+    await browser.close();
+    res.json({ data });
+  } catch (e) {
+    console.error(e.message);
+    res.json({ message: 'An error has ocurred' });
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
